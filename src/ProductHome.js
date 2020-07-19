255e22caee61de043da334fb2f93b60a29d61807
azharali8907 <60216443+azharali8907@users.noreply.github.com>
2020-07-19 12:16:29 -0700

    Update ProductHome.js

--- src/ProductHome.js
+++ src/ProductHome.js
@@ -15,18 +15,3 @@ function ProductHome() {
 
 export default ProductHome;
 
-{/*
- <ul>
-       {Object.entries(Shoes).map(([id,{name, img}]) => (
-
-        <li key={id}>
-          <Link to={`/products/${id}`}>
-          <h2>{name}</h2>
-          <img src={img} height={150} alt={name} />
-          </Link>
-        </li>
-       
-       )
-       )}
-     </ul>
-*/}
